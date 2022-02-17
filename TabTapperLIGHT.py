c7c0f702ec74bc0533db3af92d26c40e263d7d2d
KEANU_AER <71291795+keanuaer@users.noreply.github.com>
2022-02-17 19:21:29 +0100

    Add files via upload

---
+++ TabTapperLIGHT.py
@@ -0,0 +1,31 @@
+#Github: Keanuaer
+
+print("""
+
+
+""")
+print("Digi(TAB)ButtonTapper by Keanu_aer")
+print("""
+
+
+""")
+#Inputs:
+#Input Amount of Lines
+num = input ("Choose Amount: ")
+print("You choose DigiTabTapper for " + num +""" times. Here we go: 
+
+
+
+
+""")
+#VariableDefinitions  ////  Rewrite this if you want.
+print(r"""
+DigiKeyboard.write('/t');""" *int(num))
+print("""
+
+
+
+
+""")
+#for the use in Terminal
+input('Press ENTER to exit')
